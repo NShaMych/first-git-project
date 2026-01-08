4fccc4a8ddf7c897408b594a5b950e0ebc10a4d7
Nikita Shalamov <shalamovnik@yandex.ru>
2026-01-08 17:13:44 +0500

    change sout

--- src/main/java/by/javaguru/App.java
+++ src/main/java/by/javaguru/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "GIT" );
+        System.out.println( "GIT DEVELOP" );
         System.out.println("LEARNING!");
         System.out.println("It is terribly difficult");
         System.out.println("I'm happy");
